45d88ec0f09b6f99f6a4efec6a16a6242181d12c
SerVa2 <129479148+SerVa2@users.noreply.github.com>
2023-04-03 19:11:28 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,14 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 1633714108 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    n = len(x)
+    error = np.random.uniform(-29, np.exp(1), size=n) # сгенерировать ошибку измерения
+    v = x + error # скорость с учетом ошибки
+    a = np.diff(v) / 10 # расчет ускорения
+    return np.mean(a) # точечная оценка коэффициента ускорения
