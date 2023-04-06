1e5960f24f5c54f498729650837ea1a141a6cf4c
SerVa2 <129479148+SerVa2@users.noreply.github.com>
2023-04-06 21:51:27 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,9 +5,6 @@ import numpy as np
 chat_id = 1633714108 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    a = 2 * (np.mean(x) / 10)
-    return a
+    acceleration = -(np.log(1 - np.mean(x)/29))/10
+    return acceleration
     
