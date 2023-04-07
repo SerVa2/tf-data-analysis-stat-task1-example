23497904c54e0cd9b9abaffaee87d78560d95c33
SerVa2 <129479148+SerVa2@users.noreply.github.com>
2023-04-08 00:10:33 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,6 +5,6 @@ import numpy as np
 chat_id = 1633714108 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    acceleration = -(np.log(1 - np.mean(x)/29))/10
-    return acceleration
+    a = x.mean() / 10
+    return a
     
